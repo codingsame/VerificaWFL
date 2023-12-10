9d64cda2dd0316ce19c7f32b756f9db0dd157df1
codingsame <sam188y5@gmail.com>
2023-12-10 21:31:24 +0100

    Added comments and type hints

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-import requests
-from datetime import datetime
-from random import randint
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+"""Used to parse response from API"""
 import json
+from datetime import datetime
+from urllib.parse import parse_qs, urlparse
+
+import requests
+from flask import Flask, render_template, request
+
 from data import headers
 
-from flask import Flask, request, render_template, redirect
 
-# TODO: Allow user to sign up for notification by scanning
-# slip (record extraction, send notification based on data)
-#
-# Strategy: get "dataEstrazione" from response to "make_api_request"
-# and wait for time to catch up. When time is reached, make new request
-# and push out notification.
-
-
-def parse_link(qr_link: str):
+def parse_link(qr_link: str) -> tuple[str, str, str]:
+    """
+    Parses contest number, card id and contest year from QR Code URL.
+    """
     parsed_url = urlparse(qr_link)
     contest_no = parse_qs(parsed_url.query)["C"][0]
     contest_year = parse_qs(parsed_url.query)["Y"][0]
@@ -24,8 +20,11 @@ def parse_link(qr_link: str):
     return (contest_no, contest_id, contest_year)
 
 
-def make_api_request(contest_no: str, contest_year: str, contest_id: str, game: str):
-    URL = 'https://www.gntn-pgd.it/gntn-info-verifica-schedina-nlp/rest/fe/winforlife' + game
+def make_api_request(contest_no: str, contest_year: str, contest_id: str, game: str) -> str:
+    """
+    Returns response given by lottery API.
+    """
+    url = 'https://www.gntn-pgd.it/gntn-info-verifica-schedina-nlp/rest/fe/winforlife' + game
 
     json_data = {
         'concorso': {
@@ -36,29 +35,35 @@ def make_api_request(contest_no: str, contest_year: str, contest_id: str, game: 
     }
 
     response = requests.post(
-        URL,
+        url,
         headers=headers,
         json=json_data,
+        timeout=10
     )
 
     return response.text
 
 
-def calculate_win_amount(no, id, year, game):
+def calculate_win_amount(no: str, game_id: str, year: str, game: str) -> dict[str, str | int]:
+    """
+    Returns useful data to front-end, including contest number,
+    card id, contest year and, if extraction has already happened,
+    amount of money won. If extraction hasn't happened yet, returns
+    -1 as amount.    
+    """
     amount = 0
-    resp = make_api_request(no, year, id, game)
+    resp = make_api_request(no, year, game_id, game)
     d = json.loads(resp)
     timestamp_concorso = float(d["concorso"]["dataEstrazione"])
     timestamp_ora = datetime.now().timestamp()
-    print(f"Concorso: {timestamp_concorso}, ora: {timestamp_ora}")
-    if float(d["concorso"]["dataEstrazione"]) > datetime.now().timestamp() * 1000:
-        return {'contest': no, 'idschedina': id, 'gioco': game, 'amount': -1}
+    if timestamp_concorso > timestamp_ora * 1000:
+        return {'contest': no, 'idschedina': game_id, 'gioco': game, 'amount': -1}
     unparsed_weens = d["vincita"]
     for ween in unparsed_weens:
         ween_type = ween["tipoDiVincita"]
         amount += int((int(ween_type["count"]) *
                       int(ween_type["valore"])) / 100)
-    return {'contest': no, 'idschedina': id, 'gioco': game, 'amount': amount}
+    return {'contest': no, 'idschedina': game_id, 'gioco': game, 'amount': amount}
 
 
 app = Flask(__name__)
@@ -66,12 +71,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
+    """
+    Renders main page.
+    """
     return render_template('index.html')
 
 
 @app.route('/checkwin', methods=['POST'])
 def checkwin():
-    print(request.form)
+    """
+    Is called with data retrieved from card's QR Code. Returns
+    useful data that's used to fill response to client.
+    """
     form = request.form
     no = form["C"]
     y = form["Y"]
@@ -80,17 +91,17 @@ def checkwin():
     return calculate_win_amount(no, prsn, y, gt)
 
 
-@app.route('/scanner', methods=['POST'])
-def scanner():
-    return render_template("scanner.html", gametype=request.form['game'])
+# @app.route('/scanner', methods=['POST'])
+# def scanner():
+#     return render_template("scanner.html", gametype=request.form['game'])
 
 
-@app.route('/winmessage', methods=['POST'])
-def winmessage():
-    if request.form["win"] == "0":
-        return render_template("winmessage.html", amount="Nessuna vincita.")
-    else:
-        return render_template("winmessage.html", amount=f'Hai vinto: {request.form["win"]}€')
+# @app.route('/winmessage', methods=['POST'])
+# def winmessage():
+#     if request.form["win"] == "0":
+#         return render_template("winmessage.html", amount="Nessuna vincita.")
+#     else:
+#         return render_template("winmessage.html", amount=f'Hai vinto: {request.form["win"]}€')
 
 
 if __name__ == "__main__":
