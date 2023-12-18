26b181f89b705805155ad93d2f87301d28480b05
codingsame <sam188y5@gmail.com>
2023-12-18 13:06:11 +0100

    Started work on updating total amount after adding to archive otf.

--- static/script.js
+++ static/script.js
@@ -21,16 +21,16 @@ function isScannerOn() {
 
 /**
  * @typedef {Object} Card
- * @property {"classico" | "grattacieli" | "vincicasa"} gioco     - Can assume values: {'classico', 'grattacieli', 'vincicasa'} 
- * @property {String} contest   - Contest number for game
- * @property {String} id        - Indicates identification for specific card
- * @property {string} amount    - Indicates amount won.
+ * @property {"classico" | "grattacieli" | "vincicasa"} gioco Can assume values: {'classico', 'grattacieli', 'vincicasa'} 
+ * @property {String} contest Contest number for game
+ * @property {String} id Indicates identification for specific card
+ * @property {string} amount Indicates amount won.
  * Represents a game card.
  */
 
 /**
  * 
- * @param {Card} card
+ * @param {Card} card 
  * @param {HTMLTableElement} archive 
  * Adds a card to the archive.
  * Parameter archive is altered by this operation (new row is added to it.)
@@ -51,6 +51,7 @@ function addCardToArchive(card, archive) {
     contesttd.innerText = contest;
     idtd.innerText = id;
     amounttd.innerText = amount;
+    amounttd.classList.add('amount');
 
     newOutcome.appendChild(gametd);
     newOutcome.appendChild(contesttd);
@@ -61,26 +62,36 @@ function addCardToArchive(card, archive) {
 }
 
 /**
+ * Parses all amounts from table, sums them and updates total amount.
+ */
+function updateTotalAmount() {
+    let total = 0.00;
+    let winnings = document.getElementsByClassName('amount');
+    for (let winning of winnings) {
+        console.log(winning);
+        total += parseFloat(winning.innerText);
+    }
+    let winTotal = document.getElementById('winTotal')
+    winTotal.innerText = `Vincita totale: ${total.toFixed()}€`;
+    winTotal.hidden = false;
+}
+
+/**
  * Iterates through savedGames in localStorage and adds all records
  * to the archive. This function is called at startup to load all 
  * previous games. Each game is converted to JSON before being 
  * added to the archive.
  */
 function initializeArchive() {
-    let total = 0;
     const data = JSON.parse(localStorage.getItem("savedGames"));
     let outcomeList = document.getElementById('winArchive');
 
     for (let game of data) {
         let truegame = JSON.parse(game);
-
         addCardToArchive(truegame, outcomeList);
-
-        total += truegame['amount'];
     }
-    let winTotal = document.getElementById('winTotal')
-    winTotal.innerText = `Vincita totale: ${total.toFixed(2)}€`;
-    winTotal.hidden = false;
+
+    updateTotalAmount();
 }
 
 function clearArchive() {
